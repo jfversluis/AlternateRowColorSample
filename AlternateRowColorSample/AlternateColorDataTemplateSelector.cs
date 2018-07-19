672280e95311fdc1d3289b0ac7fb37610a3944fc
Gerald Versluis <gerald@verslu.is>
2018-07-19 15:50:21 +0200

    Initial commit

---
+++ AlternateRowColorSample/AlternateColorDataTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AlternateRowColorSample
+{
+	public class AlternateColorDataTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate EvenTemplate { get; set; }
+		public DataTemplate UnevenTemplate { get; set; }
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			// TODO: Maybe some more error handling here
+			return ((List<string>)((ListView)container).ItemsSource).IndexOf(item as string) % 2 == 0 ? EvenTemplate : UnevenTemplate;
+		}
+	}
+}
---
+++ AlternateRowColorSample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AlternateRowColorSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public List<string> Items { get; set; } = new List<string>()
+		{
+			"R2-D2",
+			"3CPO",
+			"Luke Skywalker",
+			"Darth Vader",
+			"Han Solo",
+			"Chewbacca",
+			"Yoda",
+			"Obi-Wan Kenobi"
+		};
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BindingContext = this;
+		}
+	}
+}
